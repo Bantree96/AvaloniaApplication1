ff16fc18877c1210bc0e11505911f5b4f24b73df
남지원 <jwnam@crevis.co.kr>
2025-07-31 09:37:57 +0900

    초기 커밋.

---
+++ AvaloniaControls/MyCustomControl.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaControls
+{
+	public class MyCustomControl : TemplatedControl
+	{
+		public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<MyCustomControl, string>(nameof(Title));
+
+		public string Title
+		{
+			get => GetValue(TitleProperty);
+			set => SetValue(TitleProperty, value);
+		}
+	}
+}
